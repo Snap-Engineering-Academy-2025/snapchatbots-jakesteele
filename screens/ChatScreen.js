0ebed84f9afb8d7f973977a002bfe469332318ee
Jake Steele <jsteele@Jakes-MacBook-Air.local>
2025-07-16 23:37:25 -0700

    refactored BasicChatbot to accept a prompt prop. Can now pass in anything via CHATBOTS array, great polymorphism. functional

--- screens/ChatScreen.js
+++ screens/ChatScreen.js
@@ -13,6 +13,15 @@ export const CHATBOTS = {
     name: "React Native Chatbot",
     imageUrl: "https://loremflickr.com/140/140",
     component: BasicChatbot,
+      prompt: [
+    {
+      role: "system",
+      // content: "You are a pop culture trivia host. Ask 5 multiple choice questions about pop culture.",
+          content:
+      "You are now EmojiMovieGPT, a reality game show where contestants play to win it all. The premise of the game is to play for 5 rounds and have the user guess the movie for a given set of emojis. You will provide a set of emojis based on a movie and the user will provide a guess. If the user is correct, they get 1 point. First, ask the user for their name and then start the show! All of your responses should be directly addressed to the player.",
+
+    }
+  ]
   },
   "BakersChatbot": {
     id: "BakersChatbot",
@@ -37,10 +46,29 @@ export const CHATBOTS = {
 export default function ChatScreen({ route }) {
   const { chatbotName } = route.params;
 
+  // const makeChatbotComponent = (chatbotName) => {
+  //   if (CHATBOTS[chatbotName]) {
+  //     const Chatbot = CHATBOTS[chatbotName].component;
+  //     return <Chatbot />;
+  //   } else {
+  //     return <Text>No Chatbot Found with name '{chatbotName}'</Text>;
+  //   }
+  // };
+
   const makeChatbotComponent = (chatbotName) => {
     if (CHATBOTS[chatbotName]) {
       const Chatbot = CHATBOTS[chatbotName].component;
-      return <Chatbot />;
+
+      // For now, hardcode the prompt to test it
+      // const testPrompt = [
+      //   {
+      //     role: "system",
+      //     content:
+      //       "You are now MovieEmojiBot. Ask the user to guess movies from emojis.",
+      //   },
+      // ];
+
+      return <Chatbot prompt={CHATBOTS[chatbotName].prompt} />;
     } else {
       return <Text>No Chatbot Found with name '{chatbotName}'</Text>;
     }
